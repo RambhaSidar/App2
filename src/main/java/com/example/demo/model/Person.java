8b82269563d2169d828b7b9eaa6ede9a51e03d75
RambhaSidar <66744971+RambhaSidar@users.noreply.github.com>
2023-03-04 23:01:39 +0530

    pojo added

--- src/main/java/com/example/demo/model/Person.java
+++ src/main/java/com/example/demo/model/Person.java
@@ -1,11 +1,46 @@
 package com.example.demo.model;
 
-import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
-@Data
+@Document(collection = "Person")
 public class Person {
+	@Id
 	private int id;
 	private String name;
 	private String subject;
+
+	public Person() {
+
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getSubject() {
+		return subject;
+	}
+
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+
+	@Override
+	public String toString() {
+		return "Person [id=" + id + ", name=" + name + ", subject=" + subject + "]";
+	}
 
 }
