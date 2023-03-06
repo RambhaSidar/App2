5f9ae6ceba4c1ccbc007b1216332a4a8c8b0f955
RambhaSidar <66744971+RambhaSidar@users.noreply.github.com>
2023-03-07 04:06:09 +0530

    new file added

--- src/main/java/com/example/repo/PersonRepository.java
+++ src/main/java/com/example/repo/PersonRepository.java
@@ -1,5 +1,7 @@
 package com.example.repo;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,8 @@ import com.example.model.Person;
 
 @Repository
 public interface PersonRepository extends JpaRepository<Person, Integer>{
+
+	List<Person> findByName(String string);
+
 
 }
---
+++ src/test/java/com/example/model/PersonTest.java
@@ -0,0 +1,57 @@
+package com.example.model;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class PersonTest {
+	Person p=new Person();
+	@Test
+	void testPerson() {
+		p=new Person();
+	}
+
+	@Test
+	void testGetId() {
+		p.setId(1);
+		assertEquals(1, p.getId());
+	}
+
+	@Test
+	void testSetId() {
+		p.setId(1);
+		assertEquals(1, p.getId());
+	}
+
+	@Test
+	void testGetName() {
+		 p.setName("Test");
+	        assertEquals("Test",p.getName());	}
+
+	@Test
+	void testSetName() {
+        p.setName("Test");
+        assertEquals("Test",p.getName());
+	}
+
+	@Test
+	void testGetSubject() {
+		  p.setName("Test");
+	        assertEquals("Test",p.getName());	
+	        }
+
+	@Test
+	void testSetSubject() {
+		p.setSubject("java");
+		assertEquals("java",p.getSubject());
+	}
+	@Test
+	void testToString() {
+		p.setId(1);
+		p.setName("test");
+		p.setSubject("java");
+		p.toString();
+
+	}
+
+}
